aa3cb9fe645117bd8d4f50d9ac75c4787d29fbda
gama-gabriel <bielmgama@gmail.com>
2024-01-11 23:56:08 -0300

    added animations to accordion

--- src/app/components/Accordion.tsx
+++ src/app/components/Accordion.tsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import arrow from '../../../public/arrow.svg'
 import Img from 'next/image'
 import Link from 'next/link'
+import { motion, AnimatePresence, stagger } from "framer-motion"
 
 const Accordion = ({ items }: {items: Array<any>}) => {
     const [openIndices, setOpenIndices] = useState([]);
@@ -12,6 +13,23 @@ const Accordion = ({ items }: {items: Array<any>}) => {
         setOpenIndices((prevIndices: any) =>
         prevIndices.includes(index) ? prevIndices.filter((i: any) => i !== index): [...prevIndices, index]
         )
+    }
+
+    const container = {
+      hidden: { opacity: 0},
+      show: {
+        opacity: 1, 
+        transition: {
+          staggerChildren: 0.05,
+          delay: 0.05
+        }
+      }
+      
+    }
+
+    const son = {
+      hidden: { opacity: 0 },
+      show: { opacity: 1 },
     }
 
   return (
@@ -25,13 +43,26 @@ const Accordion = ({ items }: {items: Array<any>}) => {
             {item.title}
             <Img src={arrow} width={10} height={10} alt='arrow' className={openIndices.includes(index) ? 'ms-6 my-1.5 float-right' : 'ms-6 my-1.5 float-right rotate-180'}></Img>
           </div>
+          <AnimatePresence>
           {openIndices.includes(index) && (
-            <ul >
+            
+            <motion.ul variants={container}
+            initial="hidden"
+            animate="show"
+            exit={{ height: 0, opacity: 0, transition: { delay: 0.05 }}}>
               {item.content.map((itemContent, contentIndex) => (
-                <Link href={`/${itemContent}`} key={contentIndex} className='flex my-ayto py-2 ms-3 text-stone-400 cursor-pointer hover:text-white'>{itemContent}</Link>
+
+                  <motion.li key={contentIndex} variants={son} >
+                    <Link href={`/${itemContent}`} key={contentIndex} className='flex my-ayto py-2 ms-3 text-stone-400 cursor-pointer hover:text-white'>
+                      {itemContent}
+                    </Link>
+                  </motion.li>
               ))}
-            </ul>
+            </motion.ul>
+            
+            
           )}
+          </AnimatePresence>
         </div>
       ))}
     </div>
