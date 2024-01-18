f43b31f1b101f6736b0e1c5e85d10a5465a780b4
gama-gabriel <bielmgama@gmail.com>
2024-01-18 20:59:16 -0300

    added types to parameters

--- src/app/components/Accordion.tsx
+++ src/app/components/Accordion.tsx
@@ -5,23 +5,25 @@ import Img from 'next/image'
 import Link from 'next/link'
 import { motion, AnimatePresence, stagger } from "framer-motion"
 
-const Accordion = ({ items }: {items: Array<any>}) => {
-    const [openIndices, setOpenIndices] = useState([]);
+const Accordion = ({ items }: {items: Array<number>}) => {
+    const [openIndices, setOpenIndices] = useState<Array<number>>([]);
 
     const toggleItem = (index: number) => 
     {
-        setOpenIndices((prevIndices: any) =>
+        setOpenIndices((prevIndices) =>
         prevIndices.includes(index) ? prevIndices.filter((i: any) => i !== index): [...prevIndices, index]
         )
     }
 
     const container = {
-      hidden: { opacity: 0},
+      hidden: { opacity: 0, height: 0, width: 0 },
       show: {
-        opacity: 1, 
+        opacity: 1,
+        height: 'auto',
+        width: 'auto', 
         transition: {
-          staggerChildren: 0.05,
-          delay: 0.05
+          staggerChildren: 0.10,
+          delay: 0.005
         }
       }
       
@@ -34,10 +36,10 @@ const Accordion = ({ items }: {items: Array<any>}) => {
 
   return (
     <div>
-      {items.map((item, index) => (
+      {items.map((item: any, index: number) => (
         <div key={index}>
           <div
-            className={openIndices.includes(index) ? 'block my-ayto py-2 text-stone-400 cursor-pointer font-bold hover:text-white' : 'block my-ayto py-2 text-stone-400 cursor-pointer hover:text-white'}
+            className={openIndices?.includes(index) ? 'block my-ayto py-2 text-stone-400 cursor-pointer font-bold hover:text-white' : 'block my-ayto py-2 text-stone-400 cursor-pointer hover:text-white'}
             onClick={() => toggleItem(index)}
           >
             {item.title}
@@ -50,7 +52,7 @@ const Accordion = ({ items }: {items: Array<any>}) => {
             initial="hidden"
             animate="show"
             exit={{ height: 0, opacity: 0, transition: { delay: 0.05 }}}>
-              {item.content.map((itemContent, contentIndex) => (
+              {item.content.map((itemContent: string, contentIndex: number) => (
 
                   <motion.li key={contentIndex} variants={son} >
                     <Link href={`/${itemContent}`} key={contentIndex} className='flex my-ayto py-2 ms-3 text-stone-400 cursor-pointer hover:text-white'>
