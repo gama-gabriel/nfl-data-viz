dc79c06a45cbf06a386ea5c83c1db5ec9f05b120
gama-gabriel <bielmgama@gmail.com>
2024-01-27 23:59:03 -0300

    revamped navbar with shacdn components

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,101 +1,227 @@
+"use client";
 
+import React from "react";
+import Acc from "./CollapsibleElement";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
 
-import React from 'react';
-import Accordion from './Accordion';
-import Balloon from './Balloon'
-import Underline from './Underline'
-import logo from '../../../public/logo.svg'
-import hamburger from '../resources/hamburger.svg'
-import Img from 'next/image'
-import Link from 'next/link'
+import Balloon from "./Balloon";
+import Underline from "./Underline";
+import logo from "../../../public/logo.svg";
+import hamburger from "../resources/hamburger.svg";
+import Img from "next/image";
+import Link from "next/link";
+import DropdownWrapper from "./DropdownWrapper";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import { cn } from "@/lib/utils";
 
+const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li className={cn("", className)}>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          {...props}
+          className="text-sm mx-auto block select-none space-y-1 rounded-md py-4 px-2 leading-none no-underline outline-none transition-colors hover:bg-neutral-950 hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:bg-neutral-950"
+        >
+          {title}
+        </a>
+      </NavigationMenuLink>
+    </li>
+  );
+});
+ListItem.displayName = "ListItem";
 
-export default function NavBar() 
-{
-    const general_items = 
-    [
-        {
-          title: 'Offensive stats', href: 'offense',
-          content: ['Pass', 'Rush', 'Dropback vs Rush', 'Offensive success rate', 'Early downs', 'Late Downs', 'Screens', 'Trick plays', 'Short yardage', 'Motion', 'No huddle'],
-        },
-        {
-          title: 'Defensive stats',  href: 'defense',
-          content: ['Pass', 'Rush', 'Dropback vs Rush', 'Offensive success rate', 'Early downs', 'Late Downs', 'Screens', 'Trick plays', 'Short yardage', 'Motion', 'No huddle'],
-        },
-    ]
+export default function NavBar() {
+  const general_items = {
+    title: "Offensive situations",
+    href: "offense",
+    content: [
+      "Pass",
+      "Rush",
+      "Dropback vs Rush",
+      "Success rate",
+      "Early downs",
+      "Late Downs",
+      "Screens",
+      "Trick plays",
+      "Short yardage",
+      "Motion",
+      "No huddle",
+    ],
+  };
 
-    const links = 
-    [
-        {href: '/', label: 'Home', dropdown: false},
-        {href: '/general', label: 'General', dropdown: true, drop_items: [{label: 'EPA', href: 'general/epa'}, {label: 'Success Rate', href: 'general/success_rate'}], accordion: true, acc_items: general_items},
-        {href: '/teams', label: 'Teams', dropdown: true, drop_items: [{label: 'EPA', href: 'general/epa'}, {label: 'Success Rate', href: 'general/success_rate'}], accordion: true, acc_items: general_items},
-        {href: '/players', label: 'Players', dropdown: true, drop_items:[{label: 'EPA', href: 'general/epa'}, {label: 'Success Rate', href: 'general/success_rate'}], accordion: true, acc_items: general_items}
-    ]
+  const links = [
+    { href: "/", label: "Home", dropdown: false, underline: "" },
+    {
+      href: "/general",
+      label: "General",
+      dropdown: true,
+      drop_items: [
+        { label: "EPA", href: "/general/epa" },
+        { label: "Success Rate", href: "/general/success_rate" },
+      ],
+      accordion: true,
+      acc_items: general_items,
+      underline: "general",
+    },
+    {
+      href: "/teams",
+      label: "Teams",
+      dropdown: true,
+      drop_items: [
+        { label: "EPA", href: "general/epa" },
+        { label: "Success Rate", href: "general/success_rate" },
+      ],
+      accordion: true,
+      acc_items: general_items,
+      underline: "teams",
+    },
+    {
+      href: "/players",
+      label: "Players",
+      dropdown: true,
+      drop_items: [
+        { label: "EPA", href: "general/epa" },
+        { label: "Success Rate", href: "/players/quarterbacks" },
+      ],
+      accordion: true,
+      acc_items: general_items,
+      underline: "players",
+    },
+  ];
 
-    return(
-        <nav className=' px-6 py-4 bg-neutral-950 border-b border-neutral-800 flex'>
-        <Img src={hamburger} width={20} height={20} alt={'Menu'} className='cursor-pointer my-auto mx-4 invisible'></Img>
-        <Img src={logo} width={80} height={80} alt={'NFL data viz logo'} className='cursor-pointer my-auto mx-4'></Img>
-        <div className='flex px-5'>
-            <ul className='flex my-auto'>
-            {links.map((link) => (
-                <li key={link.href} className='ml-5'>
-                    <div className={link.dropdown ? 'group': ''}>
-                        <Link href={link.href} className='flex relative py-3 font-bold text-sm cursor-pointer underline-offset-4 decoration-1 text-stone-400 hover:text-white'>
-                            <Underline selected={link.href}/>
-                            {link.label}
-                        </Link>
-                        
-                            {link.dropdown &&
-                            <Balloon items={link.drop_items} >
-                                {link.accordion &&
-                                <Accordion items={link.acc_items} />
-                                }
-                            </Balloon>
-                            }
-                    </div>
-                </li>
-            ))}
-            </ul>
-        </div>
-                {/* <li className='ml-4'>
+  return (
+    <div className="m-0 px-6 py-4 bg-transparent border-b border-neutral-800 w-full gap-4 flex top-0">
+      <Img
+        src={hamburger}
+        width={20}
+        height={20}
+        alt={"Menu"}
+        className="cursor-pointer my-auto mx-4 invisible"
+      ></Img>
+      <Img
+        src={logo}
+        width={70}
+        height={70}
+        alt={"NFL data viz logo"}
+        className="cursor-pointer my-auto mx-12"
+      ></Img>
+      
 
-                    <Link href='/teams' className='relative py-3 font-bold text-sm cursor-pointer underline-offset-4 decoration-1 text-white  hover:text-white'>
-                    {path === '/teams' &&
-                    (<motion.span 
-                    layoutId='underline'
-                    className='absolute left-0 top-full block -mt-3 h-[2px] w-full bg-lime-500'/>)}
+        <NavigationMenu className="">
+          <NavigationMenuList className="gap-3">
+            <NavigationMenuItem>
+              <DropdownWrapper className="">
+                <Link href="/docs" legacyBehavior passHref>
+                  <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), 'flex relative py-3 font-bold text-sm cursor-pointer underline-offset-4 decoration-1 text-stone-200', className)}>
+                    <Underline selected='/'/>
+                    <p className=''>
                         Home
-                    </Link>
-                    <Link href='/' className='relative ml-4 py-3 font-bold text-sm cursor-pointer underline-offset-4 decoration-1 text-white  hover:text-white'>
-                    {path === '/' &&
-                    (<motion.span 
-                    layoutId='underline'
-                    className='absolute left-0 top-full block -mt-3 h-[2px] w-full bg-lime-500'/>)}
-                        Home
-                    </Link>
-                </li>
-                <li className='ml-4'>
-                    <div className='group'>
-                        <Balloon items={items} name='General'></Balloon>    
-                    </div>
-                </li>
-                <li className='ml-4'>
-                <div className='group'>
-                        <Balloon items={items} name='Teams'></Balloon>    
-                    </div>
-                </li>
-                <li className='ml-4'>
-                    <Balloon items={items} name='Players'></Balloon>    
-                </li>
-            
-            </ul> */}
-
-            
-            <input type="text" placeholder='Search' className='my-auto ms-auto me-16 w-24 h-8 px-4 bg-stone-800 rounded-md text-xs '/>
-            <p className='pe-4 my-auto text-sm'>About</p>
+                    </p>
+                  </NavigationMenuLink>
+                </Link>
+              </DropdownWrapper>
+            </NavigationMenuItem>
+            </NavigationMenuList>
+        </NavigationMenu>
+        <NavigationMenu className="">
+          <NavigationMenuList className="gap-3">
+            <NavigationMenuItem>
+              <DropdownWrapper className="">
+                <NavigationMenuTrigger>
+                    General
+                </NavigationMenuTrigger>
+              </DropdownWrapper>
+              <NavigationMenuContent className=''>
+                <ul className="grid px-4 py-3 w-56">
+                  <ListItem href="/docs" title="Home" className=""></ListItem>
+                  <ListItem
+                    href="/docs/installation"
+                    title="EPA"
+                    className=""
+                  ></ListItem>
+                  <ListItem
+                    href="/docs/primitives/typography"
+                    title="Success Rate"
+                    className=""
+                  ></ListItem>
+                  <Acc items={general_items} />
+                </ul>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+            </NavigationMenuList>
+        </NavigationMenu>
+        <NavigationMenu className="">
+          <NavigationMenuList className="gap-3">
+            <NavigationMenuItem>
+              <DropdownWrapper className="">
+                <NavigationMenuTrigger>
+                    Teams
+                </NavigationMenuTrigger>
+              </DropdownWrapper>
+              <NavigationMenuContent>
+                <ul className="grid px-4 py-3 w-56">
+                  <ListItem href="/docs" title="Home" className=""></ListItem>
+                  <ListItem
+                    href="/docs/installation"
+                    title="EPA"
+                    className=""
+                  ></ListItem>
+                  <ListItem
+                    href="/docs/primitives/typography"
+                    title="Success Rate"
+                    className=""
+                  ></ListItem>
+                  <Acc items={general_items} />
+                </ul>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+            </NavigationMenuList>
+        </NavigationMenu>
+        <NavigationMenu className="">
+          <NavigationMenuList className="gap-3">
+            <NavigationMenuItem>
+              <DropdownWrapper className="">
+                <NavigationMenuTrigger>
+                    Players
+                </NavigationMenuTrigger>
+              </DropdownWrapper>
+              <NavigationMenuContent>
+                <ul className="grid px-4 py-3 w-56">
+                  <ListItem href="/docs" title="Home" className=""></ListItem>
+                  <ListItem
+                    href="/docs/installation"
+                    title="EPA"
+                    className=""
+                  ></ListItem>
+                  <ListItem
+                    href="/docs/primitives/typography"
+                    title="Success Rate"
+                    className=""
+                  ></ListItem>
+                  <Acc items={general_items} />
+                </ul>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
         
-    </nav>
-    )
+    
+    </div>
+  );
 }
-
