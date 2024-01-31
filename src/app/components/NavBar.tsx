d497a701f593643fb7591f3d34134d665170a1c4
gama-gabriel <bielmgama@gmail.com>
2024-01-31 02:34:47 -0300

    revamped navbar

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,18 +1,12 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Acc from "./CollapsibleElement";
-import {
-  Collapsible,
-  CollapsibleContent,
-  CollapsibleTrigger,
-} from "@/components/ui/collapsible";
 
-import Balloon from "./Balloon";
 import Underline from "./Underline";
 import logo from "../../../public/logo.svg";
 import hamburger from "../resources/hamburger.svg";
-import Img from "next/image";
+import Image from "next/image";
 import Link from "next/link";
 import DropdownWrapper from "./DropdownWrapper";
 import {
@@ -25,6 +19,9 @@ import {
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
 import { cn } from "@/lib/utils";
+import { useMotionValueEvent, useScroll, motion } from "framer-motion";
+
+const MotionImage = motion(Image);
 
 const ListItem = React.forwardRef<
   React.ElementRef<"a">,
@@ -105,123 +102,164 @@ export default function NavBar() {
     },
   ];
 
-  return (
-    <div className="m-0 px-6 py-4 bg-transparent border-b border-neutral-800 w-full gap-4 flex top-0">
-      <Img
-        src={hamburger}
-        width={20}
-        height={20}
-        alt={"Menu"}
-        className="cursor-pointer my-auto mx-4 invisible"
-      ></Img>
-      <Img
-        src={logo}
-        width={70}
-        height={70}
-        alt={"NFL data viz logo"}
-        className="cursor-pointer my-auto mx-12"
-      ></Img>
-      
+  const { scrollY } = useScroll();
 
-        <NavigationMenu className="">
-          <NavigationMenuList className="gap-3">
-            <NavigationMenuItem>
-              <DropdownWrapper className="">
-                <Link href="/docs" legacyBehavior passHref>
-                  <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), 'flex relative py-3 font-bold text-sm cursor-pointer underline-offset-4 decoration-1 text-stone-200', className)}>
-                    <Underline selected='/'/>
-                    <p className=''>
+  const [hidden, setHidden] = useState(false);
+
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    const previous = scrollY.getPrevious();
+    console.log(`${hidden}`);
+    if (latest > previous && latest > 150) {
+      setHidden(true);
+    } else {
+      setHidden(false);
+    }
+  });
+
+  return (
+    <motion.div
+      className="sticky top-0 bg-neutral-900 border-b border-neutral-700 text-white text-sm"
+      variants={{
+        visible: { y: 0, opacity: 1 },
+        hidden: { y: -20 },
+      }}
+      animate={hidden ? "hidden" : "visible"}
+      transition={{ ease: "easeOut", duration: 0.1 }}
+    >
+      <section className="m-0 px-6 py-2 bg-transparent w-full gap-4 flex top-0">
+        <Link className="mx-12 my-auto group" href="/">
+          <DropdownWrapper initial={0.9}>
+            <MotionImage
+              src={logo}
+              width={20}
+              height={20}
+              alt={"NFL data viz logo"}
+              className="cursor-pointer my-auto mx-12 z-10"
+              variants={{
+                visible: { scale: 1, opacity: 1 },
+                hidden: { scale: 0 },
+              }}
+              animate={hidden ? "hidden" : "visible"}
+            />
+
+            <motion.p
+              className="mx-auto my-auto w-fit"
+              variants={{
+                visible: { y: 0, opacity: 1 },
+                hidden: { y: -9 },
+              }}
+              animate={hidden ? "hidden" : "visible"}
+              transition={{ ease: "easeOut", duration: 0.1 }}
+            >
+              <span>nfl</span>
+              <span className="font-bold"> data viz</span>
+            </motion.p>
+          </DropdownWrapper>
+        </Link>
+        <motion.div
+          variants={{
+            visible: { y: 0, opacity: 1 },
+            hidden: { y: 10 },
+          }}
+          animate={hidden ? "hidden" : "visible"}
+          transition={{ ease: "easeOut", duration: 0.1 }}
+          className="flex gap-3"
+        >
+          <NavigationMenu className="">
+            <NavigationMenuList className="gap-3">
+              <NavigationMenuItem>
+                <DropdownWrapper className="">
+                  <Link href="/docs" legacyBehavior passHref>
+                    <NavigationMenuLink
+                      className={navigationMenuTriggerStyle()}
+                    >
+                      <p className="flex relative py-3 font-bold text-sm cursor-pointer underline-offset-4 decoration-1 text-stone-200">
+                        <Underline selected="/" />
                         Home
-                    </p>
-                  </NavigationMenuLink>
-                </Link>
-              </DropdownWrapper>
-            </NavigationMenuItem>
+                      </p>
+                    </NavigationMenuLink>
+                  </Link>
+                </DropdownWrapper>
+              </NavigationMenuItem>
             </NavigationMenuList>
-        </NavigationMenu>
-        <NavigationMenu className="">
-          <NavigationMenuList className="gap-3">
-            <NavigationMenuItem>
-              <DropdownWrapper className="">
-                <NavigationMenuTrigger>
-                    General
-                </NavigationMenuTrigger>
-              </DropdownWrapper>
-              <NavigationMenuContent className=''>
-                <ul className="grid px-4 py-3 w-56">
-                  <ListItem href="/docs" title="Home" className=""></ListItem>
-                  <ListItem
-                    href="/docs/installation"
-                    title="EPA"
-                    className=""
-                  ></ListItem>
-                  <ListItem
-                    href="/docs/primitives/typography"
-                    title="Success Rate"
-                    className=""
-                  ></ListItem>
-                  <Acc items={general_items} />
-                </ul>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
+          </NavigationMenu>
+          <NavigationMenu className="">
+            <NavigationMenuList className="gap-3">
+              <NavigationMenuItem>
+                <DropdownWrapper className="">
+                  <NavigationMenuTrigger>General</NavigationMenuTrigger>
+                </DropdownWrapper>
+                <NavigationMenuContent className="">
+                  <ul className="grid px-4 py-3 w-56">
+                    <ListItem href="/docs" title="Home" className=""></ListItem>
+                    <ListItem
+                      href="/docs/installation"
+                      title="EPA"
+                      className=""
+                    ></ListItem>
+                    <ListItem
+                      href="/docs/primitives/typography"
+                      title="Success Rate"
+                      className=""
+                    ></ListItem>
+                    <Acc items={general_items} />
+                  </ul>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
             </NavigationMenuList>
-        </NavigationMenu>
-        <NavigationMenu className="">
-          <NavigationMenuList className="gap-3">
-            <NavigationMenuItem>
-              <DropdownWrapper className="">
-                <NavigationMenuTrigger>
-                    Teams
-                </NavigationMenuTrigger>
-              </DropdownWrapper>
-              <NavigationMenuContent>
-                <ul className="grid px-4 py-3 w-56">
-                  <ListItem href="/docs" title="Home" className=""></ListItem>
-                  <ListItem
-                    href="/docs/installation"
-                    title="EPA"
-                    className=""
-                  ></ListItem>
-                  <ListItem
-                    href="/docs/primitives/typography"
-                    title="Success Rate"
-                    className=""
-                  ></ListItem>
-                  <Acc items={general_items} />
-                </ul>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
+          </NavigationMenu>
+          <NavigationMenu className="">
+            <NavigationMenuList className="gap-3">
+              <NavigationMenuItem>
+                <DropdownWrapper className="">
+                  <NavigationMenuTrigger>Teams</NavigationMenuTrigger>
+                </DropdownWrapper>
+                <NavigationMenuContent>
+                  <ul className="grid px-4 py-3 w-56">
+                    <ListItem href="/docs" title="Home" className=""></ListItem>
+                    <ListItem
+                      href="/docs/installation"
+                      title="EPA"
+                      className=""
+                    ></ListItem>
+                    <ListItem
+                      href="/docs/primitives/typography"
+                      title="Success Rate"
+                      className=""
+                    ></ListItem>
+                    <Acc items={general_items} />
+                  </ul>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
             </NavigationMenuList>
-        </NavigationMenu>
-        <NavigationMenu className="">
-          <NavigationMenuList className="gap-3">
-            <NavigationMenuItem>
-              <DropdownWrapper className="">
-                <NavigationMenuTrigger>
-                    Players
-                </NavigationMenuTrigger>
-              </DropdownWrapper>
-              <NavigationMenuContent>
-                <ul className="grid px-4 py-3 w-56">
-                  <ListItem href="/docs" title="Home" className=""></ListItem>
-                  <ListItem
-                    href="/docs/installation"
-                    title="EPA"
-                    className=""
-                  ></ListItem>
-                  <ListItem
-                    href="/docs/primitives/typography"
-                    title="Success Rate"
-                    className=""
-                  ></ListItem>
-                  <Acc items={general_items} />
-                </ul>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
-        
-    
-    </div>
+          </NavigationMenu>
+          <NavigationMenu className="">
+            <NavigationMenuList className="gap-3">
+              <NavigationMenuItem>
+                <DropdownWrapper className="">
+                  <NavigationMenuTrigger>Players</NavigationMenuTrigger>
+                </DropdownWrapper>
+                <NavigationMenuContent>
+                  <ul className="grid px-4 py-3 w-56">
+                    <ListItem href="/docs" title="Home" className=""></ListItem>
+                    <ListItem
+                      href="/docs/installation"
+                      title="EPA"
+                      className=""
+                    ></ListItem>
+                    <ListItem
+                      href="/docs/primitives/typography"
+                      title="Success Rate"
+                      className=""
+                    ></ListItem>
+                    <Acc items={general_items} />
+                  </ul>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+        </motion.div>
+      </section>
+    </motion.div>
   );
 }
