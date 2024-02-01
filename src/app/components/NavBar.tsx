44e6c63923c3e38390aaf264d2086ef726787345
gama-gabriel <bielmgama@gmail.com>
2024-01-31 23:45:03 -0300

    changed hrefs of links

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useRef, useState } from "react";
-import Acc from "./CollapsibleElement";
+import Acc from "./Expandable";
 
 import Underline from "./Underline";
 import logo from "../../../public/logo.svg";
@@ -22,6 +22,7 @@ import { cn } from "@/lib/utils";
 import { useMotionValueEvent, useScroll, motion } from "framer-motion";
 
 const MotionImage = motion(Image);
+
 
 const ListItem = React.forwardRef<
   React.ElementRef<"a">,
@@ -44,89 +45,70 @@ const ListItem = React.forwardRef<
 ListItem.displayName = "ListItem";
 
 export default function NavBar() {
-  const general_items = {
+  const exp_offense = {
     title: "Offensive situations",
     href: "offense",
     content: [
-      "Pass",
-      "Rush",
-      "Dropback vs Rush",
-      "Success rate",
-      "Early downs",
-      "Late Downs",
-      "Screens",
-      "Trick plays",
-      "Short yardage",
-      "Motion",
-      "No huddle",
+      {text: "Pass", ref: 'pass'},
+      {text: "Run", ref: 'run'},
+      {text: "Dropback vs Rush", ref: 'dropback_rush'},
+      {text: "Success rate", ref: 'success_rate'},
+      {text: "Early downs", ref: 'early_downs'},
+      {text: "Late Downs", ref: 'late_downs'},
+      {text: "Screens", ref: 'screens'},
+      {text: "Trick plays", ref: 'trick_plays'},
+      {text: "Short yardage", ref: 'short_yards'},
+      {text: "Motion", ref: 'motion'},
+      {text: "No huddle", ref: 'no_huddle'},
     ],
   };
 
-  const links = [
-    { href: "/", label: "Home", dropdown: false, underline: "" },
-    {
-      href: "/general",
-      label: "General",
-      dropdown: true,
-      drop_items: [
-        { label: "EPA", href: "/general/epa" },
-        { label: "Success Rate", href: "/general/success_rate" },
-      ],
-      accordion: true,
-      acc_items: general_items,
-      underline: "general",
-    },
-    {
-      href: "/teams",
-      label: "Teams",
-      dropdown: true,
-      drop_items: [
-        { label: "EPA", href: "general/epa" },
-        { label: "Success Rate", href: "general/success_rate" },
-      ],
-      accordion: true,
-      acc_items: general_items,
-      underline: "teams",
-    },
-    {
-      href: "/players",
-      label: "Players",
-      dropdown: true,
-      drop_items: [
-        { label: "EPA", href: "general/epa" },
-        { label: "Success Rate", href: "/players/quarterbacks" },
-      ],
-      accordion: true,
-      acc_items: general_items,
-      underline: "players",
-    },
-  ];
-
+  const exp_defense = {
+    title: "Defensive situations",
+    href: "defense",
+    content: [
+      {text: "Pass", ref: 'pass'},
+      {text: "Run", ref: 'run'},
+      {text: "Dropback vs Rush", ref: 'dropback_rush'},
+      {text: "Success rate", ref: 'success_rate'},
+      {text: "Early downs", ref: 'early_downs'},
+      {text: "Late Downs", ref: 'late_downs'},
+      {text: "Screens", ref: 'screens'},
+      {text: "Trick plays", ref: 'trick_plays'},
+      {text: "Short yardage", ref: 'short_yards'},
+      {text: "Motion", ref: 'motion'},
+      {text: "No huddle", ref: 'no_huddle'},
+    ],
+  };
   const { scrollY } = useScroll();
 
   const [hidden, setHidden] = useState(false);
 
   useMotionValueEvent(scrollY, "change", (latest) => {
     const previous = scrollY.getPrevious();
-    console.log(`${hidden}`);
-    if (latest > previous && latest > 150) {
-      setHidden(true);
-    } else {
-      setHidden(false);
+    if (previous != undefined) {
+      if (latest > previous && latest > 150) {
+        setHidden(true);
+      } else {
+        setHidden(false);
+      }
     }
   });
+  function template({ y, z }: { y: number; z: number }) {
+    return `translateY(${y}) translateZ(${z})`;
+  }
 
   return (
-    <motion.div
+    <motion.header
       className="sticky top-0 bg-neutral-900 border-b border-neutral-700 text-white text-sm"
       variants={{
         visible: { y: 0, opacity: 1 },
         hidden: { y: -20 },
       }}
       animate={hidden ? "hidden" : "visible"}
-      transition={{ ease: "easeOut", duration: 0.1 }}
+      transition={{ type: "spring", duration: 0.1 }}
     >
-      <section className="m-0 px-6 py-2 bg-transparent w-full gap-4 flex top-0">
+      <div className="m-0 px-6 py-2 w-full gap-4 flex">
         <Link className="mx-12 my-auto group" href="/">
           <DropdownWrapper initial={0.9}>
             <MotionImage
@@ -137,9 +119,10 @@ export default function NavBar() {
               className="cursor-pointer my-auto mx-12 z-10"
               variants={{
                 visible: { scale: 1, opacity: 1 },
-                hidden: { scale: 0 },
+                hidden: { scale: 0, opacity: 0 },
               }}
               animate={hidden ? "hidden" : "visible"}
+              transition={{ duration: 0.2 }}
             />
 
             <motion.p
@@ -158,18 +141,18 @@ export default function NavBar() {
         </Link>
         <motion.div
           variants={{
-            visible: { y: 0, opacity: 1 },
+            visible: { y: 0 },
             hidden: { y: 10 },
           }}
           animate={hidden ? "hidden" : "visible"}
-          transition={{ ease: "easeOut", duration: 0.1 }}
+          transition={{ duration: 0.1, type: "spring" }}
           className="flex gap-3"
         >
           <NavigationMenu className="">
-            <NavigationMenuList className="gap-3">
+            <NavigationMenuList>
               <NavigationMenuItem>
                 <DropdownWrapper className="">
-                  <Link href="/docs" legacyBehavior passHref>
+                  <Link href="/" legacyBehavior passHref>
                     <NavigationMenuLink
                       className={navigationMenuTriggerStyle()}
                     >
@@ -184,7 +167,7 @@ export default function NavBar() {
             </NavigationMenuList>
           </NavigationMenu>
           <NavigationMenu className="">
-            <NavigationMenuList className="gap-3">
+            <NavigationMenuList>
               <NavigationMenuItem>
                 <DropdownWrapper className="">
                   <NavigationMenuTrigger>General</NavigationMenuTrigger>
@@ -193,23 +176,25 @@ export default function NavBar() {
                   <ul className="grid px-4 py-3 w-56">
                     <ListItem href="/docs" title="Home" className=""></ListItem>
                     <ListItem
-                      href="/docs/installation"
+                      href="/general/epa"
                       title="EPA"
                       className=""
                     ></ListItem>
                     <ListItem
-                      href="/docs/primitives/typography"
+                      href="/general/success_rate"
                       title="Success Rate"
                       className=""
                     ></ListItem>
-                    <Acc items={general_items} />
+                    <Acc items={exp_offense} />
+                    <Acc items={exp_defense} />
                   </ul>
                 </NavigationMenuContent>
               </NavigationMenuItem>
             </NavigationMenuList>
           </NavigationMenu>
+
           <NavigationMenu className="">
-            <NavigationMenuList className="gap-3">
+            <NavigationMenuList>
               <NavigationMenuItem>
                 <DropdownWrapper className="">
                   <NavigationMenuTrigger>Teams</NavigationMenuTrigger>
@@ -227,12 +212,13 @@ export default function NavBar() {
                       title="Success Rate"
                       className=""
                     ></ListItem>
-                    <Acc items={general_items} />
+                    <Acc items={exp_offense} />
                   </ul>
                 </NavigationMenuContent>
               </NavigationMenuItem>
             </NavigationMenuList>
           </NavigationMenu>
+
           <NavigationMenu className="">
             <NavigationMenuList className="gap-3">
               <NavigationMenuItem>
@@ -252,14 +238,15 @@ export default function NavBar() {
                       title="Success Rate"
                       className=""
                     ></ListItem>
-                    <Acc items={general_items} />
+                    <Acc items={exp_offense} />
                   </ul>
                 </NavigationMenuContent>
               </NavigationMenuItem>
             </NavigationMenuList>
           </NavigationMenu>
+
         </motion.div>
-      </section>
-    </motion.div>
+      </div>
+    </motion.header>
   );
 }
