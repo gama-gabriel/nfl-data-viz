ac057a4c7dd6f521b7bbb0113242ec8e502b9531
gama-gabriel <bielmgama@gmail.com>
2024-02-01 22:31:59 -0300

    adapted underline for new navbar

--- src/app/components/Underline.tsx
+++ src/app/components/Underline.tsx
@@ -1,16 +1,40 @@
-'use client'
-import React from 'react'
-import { motion } from 'framer-motion'
-import { usePathname } from 'next/navigation';
+"use client";
+import React, { useEffect, useState } from "react";
+import { motion, useScroll } from "framer-motion";
+import { usePathname } from "next/navigation";
 
-export default function Underline({ selected }: {selected: string}) 
-{
-    const path = usePathname()
-    return (
-        <>
-        {path === selected &&
-        (<motion.span layoutId='underline' className='absolute left-0 top-full block rounded-lg -mt-3 h-[1px] w-full bg-lime-500'/>)}
-        </>
-    )
+export default function Underline({ selected }: { selected: string }) {
+  const { scrollY } = useScroll();
+
+  const scrollValue = scrollY.get();
+
+  const pathname = usePathname();
+  const parts = pathname.split("/");
+
+  const [prevPathname, setPrevPathname] = useState(pathname);
+
+  useEffect(() => {
+    if (pathname !== prevPathname) {
+      setPrevPathname(pathname);
+    }
+  }, [pathname, prevPathname]);
+
+  return (
+    <>
+      {selected == parts[1] && (
+        <motion.span
+          layoutId="underline"
+          className="absolute left-0 top-full block -mt-3 h-[1px] w-full bg-lime-500"
+          style={
+            pathname != prevPathname
+              ? {
+                  transform: `translateY(${scrollValue}px)`,
+                }
+              : { transform: "" }
+          }
+          aria-hidden="true"
+        />
+      )}
+    </>
+  );
 }
-
