5bc22809241e0df4233ed7ef91bd991d6ce06b62
gama-gabriel <bielmgama@gmail.com>
2024-02-15 23:34:36 -0300

    added getting started section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,16 @@ import ChartSvg from "./components/svg/ChartSvg";
 import FilterSvg from "./components/svg/FilterSvg";
 import { Separator } from "@/components/ui/separator";
 import { ArrowRightIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
 export default function Menu() {
   const ref = useRef<HTMLDivElement>(null);
   const view = useInView(ref, { margin: "-60% 0px -50% 0px" });
 
   const gridRef = useRef<HTMLDivElement>(null);
-  const gridView = useInView(gridRef, { margin: "-40% 0px -40% 0px" });
+  const gridView = useInView(gridRef);
 
   const MotionArrow = motion(ArrowRightIcon);
+  const MotionLink = motion(Link);
 
   return (
     <>
@@ -35,52 +37,104 @@ export default function Menu() {
             Visualize and navigate through various sets of NFL statistics, in a
             rich and immersive experience for football enthusiasts
           </h3>
-          <motion.button className="inline-flex items-center justify-center px-6 py-3 align-bottom w-fit mx-auto rounded-sm border border-[#16ff00] bg-transparent font-bold" variants={{hover: { scale: 1.1}}} whileHover='hover'>
-            Get started <MotionArrow color="#16ff00" className="relative ms-2 opacity-70" variants={{hover: {x: 10, opacity: 1}}}/>
+          <motion.button
+            className="inline-flex items-center justify-center px-6 py-3 align-bottom w-fit mx-auto rounded-sm border border-[#16ff00] bg-transparent font-bold"
+            variants={{ hover: { scale: 1.1 } }}
+            whileHover="hover"
+          >
+            Get started{" "}
+            <MotionArrow
+              color="#16ff00"
+              className="relative ms-2 opacity-70"
+              variants={{ hover: { x: 10, opacity: 1 } }}
+            />
           </motion.button>
         </div>
         <div className="pe-24 mx-auto">
-          <LogoSvg/>
+          <LogoSvg />
         </div>
       </motion.div>
+      <div className="pb-20" ref={gridRef}>
+        <h2 className="w-fit h-fit mx-auto py-20 text-4xl font-bold">
+          Features
+        </h2>
 
-      <h2 className='w-fit h-fit mx-auto py-20 text-4xl font-bold' ref={gridRef}>Features</h2>
+        <motion.div
+          className="flex mx-auto px-10 w-full gap-3 h-56 justify-center 2xl:w-full"
+          animate={gridView ? "rest" : "hidden"}
+        >
+          <div className="px-4 w-[calc(30%)] ">
+            <StatsSvg />
+            <h2 className="font-bold text-2xl text-center p-3 ">
+              NFL advanced stats simplified
+            </h2>
+            <p className="text-lg text-center p-3">
+              In a easy way to understand, yet extremely detailed
+            </p>
+          </div>
+          <Separator orientation="vertical" className="h-56" />
 
-      <motion.div className="flex mx-auto px-10 w-full gap-3 h-56 justify-center 2xl:w-full"  animate={ gridView ? 'rest' : 'hidden'}>
-        <div className="px-4 w-[calc(30%)] ">
-          <StatsSvg />
-          <h2 className="font-bold text-2xl text-center p-3 ">
-            NFL advanced stats simplified
-          </h2>
-          <p className="text-lg text-center p-3">
-            In a easy way to understand, yet extremely detailed
-          </p>
-        </div>
-        <Separator orientation="vertical" className="h-56" />
+          <div className="px-4 w-[calc(30%)]">
+            <ChartSvg />
+            <h2 className="font-bold text-2xl text-center p-3">
+              Dynamic and interactive charts and tables
+            </h2>
+            <p className="text-lg text-center p-3">
+              In a easy way to understand, yet extremely detailed
+            </p>
+          </div>
+          <Separator orientation="vertical" className="h-56" />
 
-        <div className="px-4 w-[calc(30%)]">
-          <ChartSvg />
-          <h2 className="font-bold text-2xl text-center p-3">
-            Dynamic and interactive charts and tables
-          </h2>
-          <p className="text-lg text-center p-3">
-            In a easy way to understand, yet extremely detailed
-          </p>
-        </div>
-        <Separator orientation="vertical" className="h-56" />
+          <div className="px-4 w-[calc(30%)]">
+            <FilterSvg />
+            <h2 className="font-bold text-2xl text-center p-3">
+              Customizable data filters
+            </h2>
+            <p className="text-lg text-center p-3">
+              In a easy way to understand, yet extremely detailed
+            </p>
+          </div>
+        </motion.div>
+      </div>
 
-        <div className="px-4 w-[calc(30%)]">
-          <FilterSvg />
-          <h2 className="font-bold text-2xl text-center p-3">
-            Customizable data filters
-          </h2>
-          <p className="text-lg text-center p-3">
-            In a easy way to understand, yet extremely detailed
-          </p>
-        </div>
-      </motion.div>
+      <div className="flex flex-col justify-center items-center py-20">
+        <h2 className="text-4xl font-bold py-2" ref={gridRef}>
+          Getting started
+        </h2>
+        <h3 className="text-xl">Start with the dataset you prefer</h3>
+      </div>
 
-      <div className=" bg-black h-screen">Texto</div>
+      <div className="flex justify-center  gap-5 items-center ">
+        <MotionLink
+          href="/general/home"
+          className="px-8 py-4 flex flex-col items-center justify-center border rounded-xl border-[#16ff00] "
+          initial="rest"
+          whileHover="hover"
+        >
+          <p className="font-bold text-xl pb-1">General</p>
+          <motion.p
+            variants={{ rest: { opacity: 0.8 }, hover: { opacity: 1 } }}
+          >
+            League-wide team stats
+          </motion.p>
+        </MotionLink>
+        <MotionLink
+          href="/teams/home"
+          className="px-8 py-4 flex flex-col items-center justify-center border rounded-xl border-[#16ff00] 
+          "
+        >
+          <p className="font-bold text-xl pb-1">Teams</p>
+          <p>League-wide team stats</p>
+        </MotionLink>
+        <MotionLink
+          href="/players/home"
+          className="px-8 py-4 flex flex-col items-center justify-center border rounded-xl border-[#16ff00] "
+        >
+          <p className="font-bold text-xl pb-1">Home</p>
+          <p>League-wide team stats</p>
+        </MotionLink>
+      </div>
+      <div className=" bg-black h-screen"></div>
     </>
   );
 }
