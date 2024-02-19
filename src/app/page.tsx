cd820931fb6ce2f54999595a367b27f4d962d99d
gama-gabriel <bielmgama@gmail.com>
2024-02-18 22:17:11 -0300

    changed hero bg and animation, changed getting started flex dir

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Menu() {
   const view = useInView(ref, { margin: "-60% 0px -50% 0px" });
 
   const gridRef = useRef<HTMLDivElement>(null);
-  const gridView = useInView(gridRef);
+  const gridView = useInView(gridRef, { margin: "10% 0px 10% 0px" });
 
   const MotionArrow = motion(ArrowRightIcon);
   const MotionLink = motion(Link);
@@ -26,12 +26,14 @@ export default function Menu() {
   return (
     <>
       <motion.div
-        className="w-full grid grid-cols-2 py-28 px-6 xl:px-10"
+        className="w-full grid grid-cols-2 py-28 px-6 bg-gradient-to-t from-transparent to-transparent xl:px-10"
         ref={ref}
         animate={
           view
-            ? { backgroundColor: "#090011", transition: { duration: 0.5 } }
-            : { backgroundColor: "#09001100", transition: { duration: 1 } }
+            ? { background:
+              "linear-gradient(to bottom, #2c0049, #090011, #000000)", transition: {duration: 5, delay: 0.5}}
+            : { background:
+              "linear-gradient(to bottom, #000000, #000000, #000000)", transition: {duration: 1}}
         }
       >
         <div className="flex flex-col px-12 max-h-96">
@@ -59,13 +61,14 @@ export default function Menu() {
           <LogoSvg />
         </div>
       </motion.div>
-      <div className="pb-20" ref={gridRef}>
+      <div className="pb-20">
         <h2 className="w-fit h-fit mx-auto py-20 text-4xl font-bold">
           Features
         </h2>
 
         <motion.div
-          className="flex mx-auto px-10 w-full gap-3 h-56 justify-center 2xl:w-full"
+          className="flex mx-auto px-10 w-full gap-3 h-56 justify-center 2xl:w-full divide-x-[1px] divide-neutral-500"
+          ref={gridRef}
           animate={gridView ? "rest" : "hidden"}
         >
           <div className="px-4 w-[calc(30%)] ">
@@ -77,7 +80,7 @@ export default function Menu() {
               In a easy way to understand, yet extremely detailed
             </p>
           </div>
-          <Separator orientation="vertical" className="h-56" />
+          
 
           <div className="px-4 w-[calc(30%)]">
             <ChartSvg />
@@ -88,7 +91,6 @@ export default function Menu() {
               In a easy way to understand, yet extremely detailed
             </p>
           </div>
-          <Separator orientation="vertical" className="h-56" />
 
           <div className="px-4 w-[calc(30%)]">
             <FilterSvg />
@@ -103,13 +105,13 @@ export default function Menu() {
       </div>
 
       <div className="flex flex-col justify-center items-center py-20">
-        <h2 className="text-4xl font-bold py-2" ref={gridRef}>
+        <h2 className="text-4xl font-bold py-2">
           Getting started
         </h2>
         <h3 className="text-xl">Start with the dataset you prefer</h3>
       </div>
 
-      <div className="flex flex-col justify-center gap-5 items-center ">
+      <div className="flex justify-center gap-5 items-center ">
         <MotionLink
           href="/general/home"
           className="px-6 py-4 flex items-center justify-center "
@@ -118,7 +120,7 @@ export default function Menu() {
         >
           <NflSvg></NflSvg>
           <div className="flex flex-col items-center justify-center ps-4">
-            <p className="font-bold text-lg self-start">General</p>
+            <motion.p className="font-bold text-lg self-start" >General</motion.p>
             <motion.p
               className="w-48"
               variants={{ rest: { opacity: 0.8 }, hover: { opacity: 1 } }}
