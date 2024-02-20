e9be48f3e4a16b6c11198c8703a1b927e3d0b24d
gama-gabriel <bielmgama@gmail.com>
2024-02-19 23:53:09 -0300

    cleaned general page

--- src/app/general/page.tsx
+++ src/app/general/page.tsx
@@ -3,8 +3,8 @@ import React from "react";
 export default function Home() {
   return (
     <>
-      <div className='flex flex-col justify-center gap-4'>
-        <h1 className='text-4xl px-12 py-10 font-bold self-start'>General</h1>
+      <div className='flex flex-col w-3/4 justify-center items-center gap-4 border-b border-neutral-600 mx-auto'>
+        <h1 className='text-4xl py-10 font-bold '>General</h1>
         
       </div>
     </>
