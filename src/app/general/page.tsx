f0f758a3a0cf666a72072c0b9d7aac24eb9c872c
gama-gabriel <bielmgama@gmail.com>
2024-02-21 02:00:46 -0300

    added tailwind prettier

--- src/app/general/page.tsx
+++ src/app/general/page.tsx
@@ -3,15 +3,19 @@ import React from "react";
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col pt-6 pb-3 w-[90%] justify-center items-center gap-1 border-b border-neutral-600 mx-auto">
-        <h1 className="text-3xl font-bold ">General</h1>
+      <div className="mx-auto flex w-[90%] flex-col items-center justify-center gap-1 border-b border-neutral-600 pb-3 pt-6">
+        <h1 className="text-4xl font-bold ">General</h1>
         <h3 className="text-lg">League-wide statistical breakdown</h3>
       </div>
-      <div className="flex items-center justify-center">
-        <h2 className="p-6 text-xl">
+      <div className="flex w-full flex-col items-center justify-center">
+        <h2 className="p-8 text-3xl">
           Explore and visualize league-wide teams data
         </h2>
-        <div className="flex flex-col w-1/3 items-center justify-center"></div>
+        <div className="flex w-[90%] columns-3 items-center justify-center bg-red-50">
+          <div className="h-36 w-full bg-red-900">Texto</div>
+          <div className="h-36 w-full bg-red-900">Texto</div>
+          <div className="h-36 w-full bg-red-600">Texto</div>
+        </div>
       </div>
     </>
   );
