2b86bdb192523c32ec897c22893361e91ff92a00
gama-gabriel <bielmgama@gmail.com>
2024-03-12 02:51:46 -0300

    created data table component

---
+++ src/app/general/epa/data-table.tsx
@@ -0,0 +1,127 @@
+"use client";
+
+import { motion, AnimatePresence } from "framer-motion";
+import * as React from "react";
+import {
+  ColumnDef,
+  SortingState,
+  flexRender,
+  getCoreRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+}
+
+const MotionBody = motion(TableBody);
+
+const MotionRow = motion(TableRow);
+
+const container = {
+  hidden: { opacity: 0, height: 0, width: "auto" },
+  show: {
+    opacity: 1,
+    height: "auto",
+    transition: {
+      staggerChildren: 0.03,
+      delay: 0.005,
+    },
+  },
+};
+const son = {
+  hidden: { opacity: 0 },
+  show: { opacity: 1 },
+};
+
+export function DataTable<TData, TValue>({
+  columns,
+  data,
+}: DataTableProps<TData, TValue>) {
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    state: {
+      sorting,
+    },
+  });
+
+  return (
+    <div className="rounded-md border">
+      <Table>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext(),
+                        )}
+                  </TableHead>
+                );
+              })}
+            </TableRow>
+          ))}
+        </TableHeader>
+
+        <AnimatePresence>
+          <MotionBody
+            variants={container}
+            initial="hidden"
+            animate="show"
+            exit={{ height: 0, opacity: 0, transition: { delay: 0 } }}
+          >
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <MotionRow
+                  variants={son}
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext(),
+                      )}
+                    </TableCell>
+                  ))}
+                </MotionRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </MotionBody>
+        </AnimatePresence>
+      </Table>
+    </div>
+  );
+}
