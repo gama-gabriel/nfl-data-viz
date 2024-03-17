9c555a86c75fd3f9205579f60cc9e31523e21456
gama-gabriel <bielmgama@gmail.com>
2024-03-16 22:17:19 -0300

    added sorted index and inverted sort to some columns

--- src/app/general/epa/columns.tsx
+++ src/app/general/epa/columns.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import { DataTableColumnHeader } from "@/components/ui/ColumnHeader";
-import { ColumnDef } from "@tanstack/react-table";
+import { ColumnDef, SortingState } from "@tanstack/react-table";
 import Image from "next/image";
+
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
 export type Epa_table = {
@@ -19,20 +20,27 @@ export type Epa_table = {
 export const columns: ColumnDef<Epa_table>[] = [
   {
     accessorKey: "team",
-    header: () => (
-      <div className="flex items-center justify-start ps-24">Team</div>
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Offensive EPA/play" />
     ),
-    cell: ({ row, table }) => {
+    cell: ({ row, column, table }) => {
       const team: Epa_table["team"] = row.getValue("team");
+      const rowModel = table.getSortedRowModel();
+      let index = 0;
+      console.log(table.getState().sorting);
+      if (table.getState().sorting[0]?.desc === false) {
+        const reversedRows = rowModel?.flatRows?.reverse();
+        index =
+          reversedRows.length -
+          1 -
+          reversedRows.findIndex((flatRow) => flatRow.id === row.id);
+      } else {
+        index = rowModel?.rows?.findIndex((flatRow) => flatRow.id === row.id);
+      }
       return (
-        <div className="flex gap-4 items-center justify-start px-2 h-full min-w-48">
-          <div className="w-6">
-            {(table
-              .getSortedRowModel()
-              ?.flatRows?.findIndex((flatRow) => flatRow.id === row.id) || 0) +
-              1}
-          </div>
-          <img
+        <div className="min-w-48 flex h-full items-center justify-start gap-4 px-2">
+          <div className="w-6">{index + 1}</div>
+          <Image
             height={200}
             width={200}
             src={team.logo}
@@ -65,7 +73,7 @@ export const columns: ColumnDef<Epa_table>[] = [
     cell: ({ row }) => {
       const off_epa = parseFloat(row.getValue<number>("off_epa").toFixed(4));
       return (
-        <div className="px-3 flex items-center justify-center">{off_epa}</div>
+        <div className="flex items-center justify-center px-3">{off_epa}</div>
       );
     },
   },
@@ -78,9 +86,11 @@ export const columns: ColumnDef<Epa_table>[] = [
     cell: ({ row }) => {
       const def_epa = parseFloat(row.getValue<number>("def_epa").toFixed(4));
       return (
-        <div className="px-3 flex items-center justify-center">{def_epa}</div>
+        <div className="flex items-center justify-center px-3">{def_epa}</div>
       );
     },
+    invertSorting: true,
+    sortDescFirst: true,
   },
 
   {
@@ -93,7 +103,7 @@ export const columns: ColumnDef<Epa_table>[] = [
         row.getValue<number>("off_total_epa").toFixed(4),
       );
       return (
-        <div className="px-3 flex items-center justify-center">
+        <div className="flex items-center justify-center px-3">
           {off_total_epa}
         </div>
       );
@@ -109,7 +119,7 @@ export const columns: ColumnDef<Epa_table>[] = [
         row.getValue<number>("def_total_epa").toFixed(4),
       );
       return (
-        <div className="px-3 flex items-center justify-center">
+        <div className="flex items-center justify-center px-3">
           {def_total_epa}
         </div>
       );
@@ -125,7 +135,7 @@ export const columns: ColumnDef<Epa_table>[] = [
       const off_total_plays: Epa_table["off_total_plays"] =
         row.getValue("off_total_plays");
       return (
-        <div className="px-3 flex items-center justify-center">
+        <div className="flex items-center justify-center px-3">
           {off_total_plays}
         </div>
       );
@@ -141,7 +151,7 @@ export const columns: ColumnDef<Epa_table>[] = [
       const def_total_plays: Epa_table["def_total_plays"] =
         row.getValue("def_total_plays");
       return (
-        <div className="px-3 flex items-center justify-center">
+        <div className="flex items-center justify-center px-3">
           {def_total_plays}
         </div>
       );
