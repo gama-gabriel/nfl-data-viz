167886c2070f43efa012c16416246d2c2a2a27c5
gama-gabriel <bielmgama@gmail.com>
2024-03-25 00:34:35 -0300

    added theme switch to navbar

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Acc from "./Expandable";
 import Underline from "./Underline";
 import logo from "../../../public/logo.svg";
@@ -18,8 +18,8 @@ import {
 } from "@/components/ui/navigation-menu";
 import { cn } from "@/lib/utils";
 import { useMotionValueEvent, useScroll, motion } from "framer-motion";
-import { SunIcon } from "@radix-ui/react-icons";
-import { MoonIcon } from "@radix-ui/react-icons";
+import { SunIcon, MoonIcon, CircleIcon } from "@radix-ui/react-icons";
+import { useTheme } from "next-themes";
 const MotionImage = motion(Image);
 
 const ListItem = React.forwardRef<
@@ -33,7 +33,7 @@ const ListItem = React.forwardRef<
           ref={ref}
           {...props}
           href={props.href!}
-          className="hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground mx-auto block select-none space-y-1 rounded-md px-2 py-4 text-sm leading-none no-underline outline-none transition-colors hover:bg-neutral-950 focus:bg-neutral-950"
+          className="hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground mx-auto block select-none space-y-1 rounded-md px-2 py-4 text-sm font-medium leading-none  no-underline outline-none transition-colors hover:bg-neutral-300 focus:bg-neutral-300  dark:hover:bg-black dark:focus:bg-black"
         >
           {title}
         </Link>
@@ -42,6 +42,67 @@ const ListItem = React.forwardRef<
   );
 });
 ListItem.displayName = "ListItem";
+
+const ThemeSwitch = () => {
+  const [mounted, setMounted] = useState(false);
+  const { setTheme, resolvedTheme } = useTheme();
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted)
+    return (
+      <div role="status">
+        <svg
+          aria-hidden="true"
+          className="h-5 w-5 animate-spin fill-blue-600/50 text-gray-200 dark:text-gray-600"
+          viewBox="0 0 100 101"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+            fill="currentColor"
+          />
+          <path
+            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+            fill="currentFill"
+          />
+        </svg>
+        <span className="sr-only">Loading...</span>
+      </div>
+    );
+
+  if (resolvedTheme === "dark") {
+    return (
+      <motion.button
+        initial={{ scale: 0, opacity: 0.7 }}
+        animate={{
+          scale: 1,
+          transition: { scale: { delay: 0.5, duration: 0.5 } },
+        }}
+        whileHover={{ opacity: 1 }}
+        onClick={() => setTheme("light")}
+      >
+        <SunIcon color="#ffffff" className="h-5 w-5" />{" "}
+      </motion.button>
+    );
+  }
+
+  if (resolvedTheme === "light") {
+    return (
+      <motion.button
+        initial={{ scale: 0, opacity: 0.7 }}
+        animate={{
+          scale: 1,
+          transition: { scale: { delay: 0.5, duration: 0.5 } },
+        }}
+        whileHover={{ opacity: 1 }}
+        onClick={() => setTheme("dark")}
+      >
+        <MoonIcon color="#000000" className="h-5 w-5" />{" "}
+      </motion.button>
+    );
+  }
+};
 
 export default function NavBar() {
   const exp_offense = {
@@ -79,6 +140,7 @@ export default function NavBar() {
       { text: "No huddle", ref: "no_huddle" },
     ],
   };
+
   const { scrollY } = useScroll();
 
   const [hidden, setHidden] = useState(false);
@@ -96,13 +158,13 @@ export default function NavBar() {
 
   return (
     <motion.header
-      className="sticky top-0 z-10 w-full border-b border-neutral-700 bg-black/95 text-sm text-white backdrop-blur supports-[backdrop-filter]:bg-black/60"
+      className="sticky top-0 z-10 w-full border-b border-neutral-400 bg-white/5 text-sm  backdrop-blur supports-[backdrop-filter]:bg-white/10 dark:border-neutral-700 dark:bg-black/95  dark:supports-[backdrop-filter]:bg-black/60"
       variants={{
         visible: { y: 0, opacity: 1 },
         hidden: { y: -20 },
       }}
       animate={hidden ? "hidden" : "visible"}
-      transition={{ duration: 0.3 }}
+      transition={{ duration: 0.3, delay: 0.2 }}
     >
       <div className="flex items-center px-10 py-2">
         <div className="flex gap-4 ">
@@ -123,11 +185,11 @@ export default function NavBar() {
                   hidden: {
                     scale: 0,
                     opacity: 0,
-                    transition: { duration: 0.3 },
+                    transition: { duration: 0.3, delay: 0.2 },
                   },
                 }}
                 animate={hidden ? "hidden" : "visible"}
-                transition={{ duration: 0.3 }}
+                transition={{ duration: 0.3, delay: 0.2 }}
               />
 
               <motion.p
@@ -137,7 +199,7 @@ export default function NavBar() {
                   hidden: { y: -9 },
                 }}
                 animate={hidden ? "hidden" : "visible"}
-                transition={{ duration: 0.3 }}
+                transition={{ duration: 0.3, delay: 0.2 }}
               >
                 <span>nfl</span>
                 <span className="font-bold"> data viz</span>
@@ -150,7 +212,7 @@ export default function NavBar() {
               hidden: { y: 10 },
             }}
             animate={hidden ? "hidden" : "visible"}
-            transition={{ duration: 0.3 }}
+            transition={{ duration: 0.3, delay: 0.2 }}
             className="flex gap-3"
           >
             <NavigationMenu className="">
@@ -161,7 +223,7 @@ export default function NavBar() {
                       <NavigationMenuLink
                         className={navigationMenuTriggerStyle()}
                       >
-                        <div className="relative flex cursor-pointer py-3 text-sm font-bold text-stone-200 decoration-1 underline-offset-4">
+                        <div className="relative flex cursor-pointer py-3 text-sm font-bold  decoration-1 underline-offset-4 ">
                           <Underline selected="" />
                           Home
                         </div>
@@ -176,7 +238,7 @@ export default function NavBar() {
                 <NavigationMenuItem>
                   <NavitemWrapper className="">
                     <NavigationMenuTrigger>
-                      <div className="relative flex cursor-pointer py-3 text-sm font-bold text-stone-200 decoration-1 underline-offset-4">
+                      <div className="relative flex cursor-pointer py-3 text-sm font-bold  decoration-1 underline-offset-4 ">
                         <Underline selected={"general"} />
                         General
                       </div>
@@ -212,7 +274,7 @@ export default function NavBar() {
                 <NavigationMenuItem>
                   <NavitemWrapper className="">
                     <NavigationMenuTrigger>
-                      <div className="relative flex cursor-pointer py-3 text-sm font-bold text-stone-200 decoration-1 underline-offset-4">
+                      <div className="relative flex cursor-pointer py-3 text-sm font-bold decoration-1 underline-offset-4 ">
                         <Underline selected={"teams"} />
                         Teams
                       </div>
@@ -247,7 +309,7 @@ export default function NavBar() {
                 <NavigationMenuItem>
                   <NavitemWrapper className="">
                     <NavigationMenuTrigger>
-                      <div className="relative flex cursor-pointer py-3 text-sm font-bold text-stone-200 decoration-1 underline-offset-4">
+                      <div className="relative flex cursor-pointer py-3 text-sm font-bold  decoration-1 underline-offset-4 ">
                         <Underline selected={"players"} />
                         Players
                       </div>
@@ -284,13 +346,13 @@ export default function NavBar() {
             hidden: { y: 10 },
           }}
           animate={hidden ? "hidden" : "visible"}
-          transition={{ duration: 0.3 }}
-          className="flex w-full justify-end gap-3 "
+          transition={{ duration: 0.3, delay: 0.2 }}
+          className="flex w-full justify-end gap-3 px-3"
         >
-          <SunIcon className="h-5 w-5 " color="#ffffff" />
-          <NavitemWrapper>
-          <p className="px-4 font-bold">About</p>
+          <NavitemWrapper className="flex items-center justify-center">
+            <p className="px-4 font-bold">About</p>
           </NavitemWrapper>
+          <ThemeSwitch />
         </motion.div>
       </div>
     </motion.header>
