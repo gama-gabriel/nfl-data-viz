4e46b7b78b5e6109743dc465282b9518884aceaf
gama-gabriel <bielmgama@gmail.com>
2024-07-19 23:27:34 -0300

    finnaly fixed translateY bug

--- src/app/components/Underline.tsx
+++ src/app/components/Underline.tsx
@@ -1,29 +1,19 @@
 "use client";
-import { motion, useScroll } from "framer-motion";
+import { motion } from "framer-motion";
 import { usePathname } from "next/navigation";
-import { useState, useEffect } from "react";
 
 const Underline = ({ selected }: { selected: string }) => {
   const pathname = usePathname();
-   const parts = pathname.split("/");
-  const [prevPathname, setPrevPathname] = useState(pathname);
-
-  const { scrollY } = useScroll();
-
-  useEffect(() => {
-    if (pathname !== prevPathname) {
-      setPrevPathname(pathname);
-    }
-  }, [pathname, prevPathname]);
-
+  const parts = pathname.split("/");
+  
   return (
     parts[1] == selected && (
       <motion.span
+        layout
         layoutId="underline"
-        className="absolute left-0 top-full block -mt-3 h-[1px] w-full bg-[#16ff00]"
-        style={
-          pathname != prevPathname ? { translateY: scrollY.get() } : { translateY: 0 }
-        }
+        className="absolute left-0 top-full -mt-3 block h-[1px] w-full bg-[#16ff00]"
+        aria-hidden="true"
+        style={{ originY: "0px" }}
       />
     )
   );
