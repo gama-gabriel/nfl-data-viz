ca47006b62013af12cc7bb7e88b604a403e729b0
Gabriel <bielmgama@gmail.com>
2024-07-23 01:47:44 -0300

    fixed file name

--- next.config.js
+++ next.config.js
@@ -26,11 +26,11 @@ module.exports = {
   experimental: {
     outputFileTracingExcludes: {
       '*': [
-        "**/node_modules/@next/swc-core-linux-x64-gnu/**",
-        "**/node_modules/@next/swc-core-linux-x64-musl/**",
+        "**/node_modules/@next/swc-linux-x64-gnu/**",
+        "**/node_modules/@next/swc-linux-x64-musl/**",
         '**/{__pycache__,testing,tests,test_}**/**',
-        "node_modules/@next/swc-core-linux-x64-gnu/**",
-        "node_modules/@next/swc-core-linux-x64-musl/**",
+        "node_modules/@next/swc-linux-x64-gnu/**",
+        "node_modules/@next/swc-linux-x64-musl/**",
         '{__pycache__,testing,tests,test_}**/**',
         'node_modules/@swc/core-linux-x64-gnu/**',
         'node_modules/@swc/core-linux-x64-musl/**',
